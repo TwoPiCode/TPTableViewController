252b5f32d3e02b17bcb875329cbfdcaf1b34293e
James Rickett <jrickett@twopicode.com.au>
2018-10-22 09:00:18 +1100

    more changes

--- TPTableViewController/Classes/TPTableViewController.swift
+++ TPTableViewController/Classes/TPTableViewController.swift
@@ -13,7 +13,8 @@ import UIKit
 open class TPTableViewController: UIViewController {
 
     public var tableView = UITableView()
-    public var segmentedControl = UISegmentedControl()
+
+    public var backgroundColor = UIColor.groupTableViewBackground
 
     public var data = [TPTableData]() {
         didSet {
@@ -44,8 +45,12 @@ open class TPTableViewController: UIViewController {
     }
 
     // TODO: enable switching
-    var segmentedControlEnabled = true
+    public var scopeStrings: [String] = []
+    public var segmentedControl = UISegmentedControl()
 
+    // Wrapper views
+    var searchWrapperView = UIView()
+    var segmentedControlWrapperView = UIView()
 
     // search bar
     public var searchTerms = ""
@@ -65,6 +70,8 @@ open class TPTableViewController: UIViewController {
 
         }
     }
+
+    open weak var segmentedControlDelegate: TPTableViewSegmentedControlDelegate?
 
     var releaseToRefreshText: String {
         if self.delegate?.itemName != nil, let text = delegate?.itemName?() {
@@ -112,6 +119,9 @@ open class TPTableViewController: UIViewController {
         if let itemName = delegate?.itemName?() {
             title = itemName
         }
+
+        self.setupSearchBar()
+
     }
 
     var hasSetupTable = false
@@ -122,10 +132,97 @@ open class TPTableViewController: UIViewController {
         if !self.hasSetupTable {
             self.hasSetupTable = true
 
+
             self.setupTableView()
-            self.setupSearchBar()
             self.setupRefreshControl()
-            self.refreshData()
+
+            self.layoutView()
+
+            // TODO: should work out how to do this without a delay.
+            // Probably an issue of where the refresh control starts off, then the tableview scrolls to that point
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                self.refreshData()
+            }
+        }
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // This was causing some weird transitions with the navigation controller pushing and popping
+        // Not sure if this should be placed in view will appear, or safe to just remove entirely.
+        // UI looks to be otherwise ok at the moment.
+
+        //        if let navBar = navigationController?.navigationBar {
+        //            // To get transparent navigationBar
+        //            navBar.setBackgroundImage(UIImage(), for: UIBarPosition.any, barMetrics: UIBarMetrics.default)
+        //            // To remove black hairline under the Navigationbar
+        //            navBar.shadowImage = UIImage()
+        //            navBar.isTranslucent = false
+        //        }
+
+        //        self.searchController.searchBar.barTintColor = backgroundColor
+        //        self.searchController.searchBar.backgroundColor = self.backgroundColor
+
+        // bit of a hack
+        //        self.searchController.searchBar.layer.borderWidth = 1
+        //        self.searchController.searchBar.layer.borderColor = self.backgroundColor.cgColor
+    }
+
+    func layoutView() {
+
+        // these constraints are always needed
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        self.tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        // if iOS <11 then we need to layout the searchbar
+        if #available(iOS 11, *) {
+
+        } else {
+            let searchBar = searchController.searchBar
+            searchWrapperView.topAnchor.constraint(equalTo: searchBar.topAnchor).isActive = true
+            searchWrapperView.leftAnchor.constraint(equalTo: searchBar.leftAnchor).isActive = true
+            searchWrapperView.rightAnchor.constraint(equalTo: searchBar.rightAnchor).isActive = true
+            searchWrapperView.bottomAnchor.constraint(equalTo: searchBar.bottomAnchor).isActive = true
+
+            searchWrapperView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+            searchWrapperView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+            searchWrapperView.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor).isActive = true
+        }
+
+        // if we're showing a scope selector, then we want to add it below the navbar
+        if self.scopeStrings.count != 0 {
+            self.segmentedControlWrapperView.translatesAutoresizingMaskIntoConstraints = false
+            self.segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+
+            self.segmentedControlWrapperView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+            self.segmentedControlWrapperView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+            // If iOS >= 11 searchbar bottom is the same as topAnchor
+            if #available(iOS 11, *) {
+                segmentedControlWrapperView.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor).isActive = true
+            } else {
+                self.segmentedControlWrapperView.topAnchor.constraint(equalTo: self.searchWrapperView.bottomAnchor).isActive = true
+            }
+
+            self.segmentedControl.centerXAnchor.constraint(equalTo: self.segmentedControlWrapperView.centerXAnchor).isActive = true
+            self.segmentedControl.centerYAnchor.constraint(equalTo: self.segmentedControlWrapperView.centerYAnchor).isActive = true
+            self.segmentedControl.topAnchor.constraint(equalTo: self.segmentedControlWrapperView.topAnchor, constant: 8).isActive = true
+            self.segmentedControl.bottomAnchor.constraint(equalTo: self.segmentedControlWrapperView.bottomAnchor, constant: -8).isActive = true
+            self.segmentedControl.leftAnchor.constraint(equalTo: self.segmentedControlWrapperView.leftAnchor, constant: 8).isActive = true
+            self.segmentedControl.rightAnchor.constraint(equalTo: self.segmentedControlWrapperView.rightAnchor, constant: -8).isActive = true
+
+            self.tableView.topAnchor.constraint(equalTo: self.segmentedControlWrapperView.bottomAnchor).isActive = true
+        } else {
+            // tableview top is searchbar bottom
+            // If iOS >= 11 searchbar bottom is the same as topAnchor
+            if #available(iOS 11, *) {
+                tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            } else {
+                self.tableView.topAnchor.constraint(equalTo: self.searchWrapperView.bottomAnchor).isActive = true
+            }
         }
     }
 
@@ -136,72 +233,85 @@ open class TPTableViewController: UIViewController {
         self.tableView.backgroundColor = UIColor.groupTableViewBackground
         self.tableView.keyboardDismissMode = .onDrag
 
+        self.tableView.rowHeight = UITableView.automaticDimension
+        // It'd be a good idea to set this in the subclasses, and make sure this doesn't override it
+        self.tableView.estimatedRowHeight = 44
+
         let footerFrame = CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: 1)
         self.tableView.tableFooterView = UIView(frame: footerFrame)
 
         // Start: Add tableview, add constraints
         view.addSubview(self.tableView)
 
-        self.tableView.translatesAutoresizingMaskIntoConstraints = false
-
-        if segmentedControlEnabled {
-            segmentedControl = UISegmentedControl(items: ["One", "Two"])
-            view.addSubview(segmentedControl)
-            //            segmentedControl.insertSegment(withTitle: "One", at: 0, animated: true)
-            //            segmentedControl.insertSegment(withTitle: "Two", at: 1, animated: true)
-            //            segmentedControl.setTitle("One", forSegmentAt: 0)
-            //            segmentedControl.setTitle("Two", forSegmentAt: 1)
-
-            segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-
-            if #available(iOS 11.0, *) {
-                segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-            } else {
-                segmentedControl.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-            }
-            segmentedControl.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-            segmentedControl.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-            self.tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor).isActive = true
-        } else {
-            self.tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        if self.scopeStrings.count != 0 {
+            self.setupSegmentedControl()
         }
 
-        self.tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        self.tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        self.tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
         self.tableView.allowsSelection = self.delegate?.didSelectRowAt != nil
+    }
+
+    func setupSegmentedControl() {
+        self.segmentedControlWrapperView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 1)) // will get set in autolayout
+
+        self.segmentedControlWrapperView.backgroundColor = backgroundColor
+        view.addSubview(self.segmentedControlWrapperView)
+
+        self.segmentedControl = UISegmentedControl(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        for (index, scope) in self.scopeStrings.enumerated() {
+            self.segmentedControl.insertSegment(withTitle: scope, at: index, animated: false)
+        }
+        self.segmentedControl.selectedSegmentIndex = 0
+
+        self.segmentedControl.tintColor = UIColor(red: 0.92, green: 0.67, blue: 0.01, alpha: 1.00)
+        self.segmentedControl.addTarget(self, action: #selector(self.segmentedControlDidChange(_:)), for: .valueChanged)
+
+        self.segmentedControlWrapperView.addSubview(self.segmentedControl)
+
+        edgesForExtendedLayout = [.bottom, .left, .right]
+    }
+
+    // Add search for iOS10
+    func setupSearchbariOS10() {
+        self.searchWrapperView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 1)) // will get set in autolayout
+        self.searchWrapperView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(self.searchWrapperView)
+
+        let searchBar = self.searchController.searchBar
+        searchController.searchBar.barTintColor = backgroundColor
+        searchController.searchBar.backgroundColor = backgroundColor
+        searchController.searchBar.isTranslucent = false
+
+        searchWrapperView.addSubview(searchBar)
+
+        edgesForExtendedLayout = [.bottom, .left, .right]
+    }
+
+    @objc func segmentedControlDidChange(_ sender: UISegmentedControl) {
+        self.segmentedControlDelegate?.segmentedControlDidChange?(index: sender.selectedSegmentIndex, nil)
     }
 
     public func setupSearchBar() {
         // TODO: check if search bar should be added
         self.searchController = UISearchController(searchResultsController: nil)
         self.searchController.searchBar.delegate = self
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = false
+        self.searchController.searchBar.sizeToFit()
+        self.searchController.delegate = self
 
         if #available(iOS 11, *) {
             navigationItem.searchController = searchController
-            navigationItem.largeTitleDisplayMode = .never
-        }
-
-        if let textField = self.searchController.searchBar.value(forKey: "_searchField") as? UITextField {
-            textField.clearButtonMode = .always
-        }
-
-        self.searchController.dimsBackgroundDuringPresentation = false
-
-        // Default behaviour is searchbar is hidden until you pull down, so persist it
-        if #available(iOS 11, *) {
+            // Default behaviour is searchbar is hidden until you pull down, so persist it
             navigationItem.hidesSearchBarWhenScrolling = false
+        } else {
+            self.setupSearchbariOS10()
         }
 
         definesPresentationContext = true
 
-        // scopes
-        //        self.searchController.searchBar.scopeButtonTitles = scopes
-        //        self.searchController.searchBar.showsScopeBar = true
-
-        self.searchController.searchBar.sizeToFit()
-        self.searchController.delegate = self
+        if let textField = self.searchController.searchBar.value(forKey: "_searchField") as? UITextField {
+            textField.clearButtonMode = .always
+        }
     }
 
     func setupRefreshControl() {
@@ -214,7 +324,9 @@ open class TPTableViewController: UIViewController {
         // this is important for getting the layout right, otherwise the refresh controls text can get cut off
         self.extendedLayoutIncludesOpaqueBars = true
 
-        //        self.manuallyShowRefreshControl()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.manuallyShowRefreshControl()
+        }
     }
 
     func manuallyShowRefreshControl() {
@@ -224,12 +336,17 @@ open class TPTableViewController: UIViewController {
 
             let yOffsetTable = self.tableView.contentOffset.y
             if yOffsetTable < CGFloat(Float.ulpOfOne) {
-                UIView.animate(withDuration: 0.25, delay: 0, options: UIView.AnimationOptions.beginFromCurrentState, animations: {
-                    self.refreshControl.attributedTitle = NSAttributedString(string: self.refreshingDataText, attributes: [:])
-                    let refreshControlHeight = self.refreshControl.frame.height
-                    self.tableView.contentOffset = CGPoint(x: 0, y: -refreshControlHeight * 4)
+                UIView.animate(withDuration: 0.25,
+                               delay: 0,
+                               options: UIView.AnimationOptions.beginFromCurrentState,
+                               animations: {
+                                self.refreshControl.attributedTitle = NSAttributedString(string: self.refreshingDataText,
+                                                                                         attributes: [:])
+                                let refreshControlHeight = self.refreshControl.frame.height
+                                self.tableView.contentOffset = CGPoint(x: 0, y: -refreshControlHeight * 4)
 
-                }, completion: nil)
+                },
+                               completion: nil)
             }
         }
     }
@@ -300,8 +417,9 @@ open class TPTableViewController: UIViewController {
 
     // Happens when view loads for the first time or the user drags down to refresh
     public func refreshData() {
-        self.setNoContentLabel()
         self.isFetchingData = true
+        self.setNoContentLabel()
+
         self.refreshControl.layoutIfNeeded()
         self.refreshControl.beginRefreshing()
 
@@ -314,6 +432,7 @@ open class TPTableViewController: UIViewController {
             self.delegate?.loadPaginatedData?(page: 1, limit: self.itemsPerPage, query: self.searchTerms) {
                 DispatchQueue.main.async {
                     self.loadingDataEnded()
+
                 }
             }
         } else {
@@ -483,6 +602,10 @@ public protocol TPTableViewDataSource: class {
     @objc optional func didChangeScope(scopeIndex: Int, _ completion: (() -> Void)!)
 }
 
+@objc public protocol TPTableViewSegmentedControlDelegate: class {
+    @objc optional func segmentedControlDidChange(index: Int, _ completion: (() -> Void)!)
+}
+
 extension UISearchBar {
 
     private var textField: UITextField? {
@@ -531,27 +654,26 @@ extension UISearchBar {
     }
 }
 
-extension UIViewController{
-
+extension UIViewController {
 
     /// Calculate top distance with "navigationBar" and "statusBar" by adding a
     /// subview constraint to navigationBar or to topAnchor or superview
     /// - Returns: The real distance between topViewController and Bottom navigationBar
-    func calculateTopDistance() -> CGFloat{
+    func calculateTopDistance() -> CGFloat {
 
         /// Create view for misure
-        let misureView : UIView     = UIView()
-        misureView.backgroundColor  = .clear
+        let misureView: UIView = UIView()
+        misureView.backgroundColor = .clear
         view.addSubview(misureView)
 
         /// Add needed constraint
-        misureView.translatesAutoresizingMaskIntoConstraints                    = false
-        misureView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive     = true
-        misureView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive   = true
+        misureView.translatesAutoresizingMaskIntoConstraints = false
+        misureView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        misureView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         misureView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         if let nav = navigationController {
             misureView.topAnchor.constraint(equalTo: nav.navigationBar.bottomAnchor).isActive = true
-        }else{
+        } else {
             misureView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         }
 
